3407b50373adc71953db076d016efba68c7ff699
Garrett Roberts <garrettmroberts@gmail.com>
2020-03-06 14:15:18 -0600

    fallback map for users without location services enabled

--- resources/scripts/mapScript.js
+++ resources/scripts/mapScript.js
@@ -1,12 +1,28 @@
 
 // Gets user coords in order to center map
-navigator.geolocation.getCurrentPosition(function(res) {
-  var lat = res.coords.latitude;
-  var long = res.coords.longitude;
-  var userCoords = [lat, long];
-
-  // Creates a new map using user coords
-  var mymap = L.map('map').setView(userCoords, 12);
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(res) {
+    var lat = res.coords.latitude;
+    var long = res.coords.longitude;
+    var userCoords = [lat, long];
+  
+    // Creates a new map using user coords
+    var mymap = L.map('map').setView(userCoords, 12);
+  
+    // Styles newly created map
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/dark-v10',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiZ2FycmV0dG1yb2JlcnRzIiwiYSI6ImNrN2Rxcm4zYjAwbmkzZm50NXh5d211MnEifQ.QfPRfpUkyKGIjSdjfAzMkg'
+    }).addTo(mymap);
+  });
+} else {
+  // Creates a new map using generic coords
+  var userCoords = ["38.937661", "-101.904738"]
+  var mymap = L.map('map').setView(userCoords, 15);
 
   // Styles newly created map
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -17,4 +33,4 @@ navigator.geolocation.getCurrentPosition(function(res) {
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZ2FycmV0dG1yb2JlcnRzIiwiYSI6ImNrN2Rxcm4zYjAwbmkzZm50NXh5d211MnEifQ.QfPRfpUkyKGIjSdjfAzMkg'
   }).addTo(mymap);
-});
+};
